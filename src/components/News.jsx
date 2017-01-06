8e123ca86928ab67c3ab70ca3bd42943a910be8d
Kirk <kirkdmcm@gmail.com>
2017-01-06 16:08:47 -0600

    first commit

---
+++ src/components/News.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Page1 = React.createClass({
+    render: function() {
+        return (
+            <h1>News!</h1>
+        );
+    }
+});
+
+module.exports = Page1;
---
+++ src/components/Photos.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Page1 = React.createClass({
+    render: function() {
+        return (
+            <h1>Photos!</h1>
+        );
+    }
+});
+
+module.exports = Page1;
